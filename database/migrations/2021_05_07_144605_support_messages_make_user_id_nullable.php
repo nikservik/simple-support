bcc59558adf0f619f673a80bac1e8b3f6dcc8d5d
Sergey Nikiforov <ser.nikiforov@gmail.com>
2021-05-15 21:51:18 +0200

    fix migrations

--- database/migrations/2021_05_07_144605_support_messages_make_user_id_nullable.php
+++ database/migrations/2021_05_07_144605_support_messages_make_user_id_nullable.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class SupportMessagesMakeUserIdNullable extends Migration
 {
@@ -17,6 +18,9 @@ class SupportMessagesMakeUserIdNullable extends Migration
             $table->integer('user_id')->unsigned()->nullable()->change();
             $table->integer('reply_to')->unsigned()->nullable();
             $table->dropColumn('status');
+            if ($this->tableHasForeign('support_messages', 'support_dialog_id')) {
+                $table->dropForeign(['support_dialog_id']);
+            }
             $table->dropColumn('support_dialog_id');
         });
     }
@@ -31,7 +35,25 @@ class SupportMessagesMakeUserIdNullable extends Migration
         Schema::table('support_messages', function (Blueprint $table) {
             $table->integer('user_id')->unsigned()->change();
             $table->string('status')->nullable();
+            $table->integer('support_dialog_id')->unsigned();
             $table->dropColumn('reply_to');
         });
+    }
+
+    public function tableHasForeign(string $table, string $column): bool
+    {
+        $conn = Schema::getConnection()->getDoctrineSchemaManager();
+
+        $foreigns = array_map(function($key) {
+            return $key->getName();
+        }, $conn->listTableForeignKeys($table));
+
+        foreach ($foreigns as $foreign) {
+            if (Str::contains($foreign, $column)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
